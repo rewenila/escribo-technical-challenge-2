088f350e083839cf8a68413c3fd4df4d965c0023
Aline Werner <alinew10@gmail.com>
2023-11-24 22:08:52 -0300

    completed challenge

---
+++ index.js
@@ -0,0 +1,4 @@
+const somatorioDivisiveis = require('./somatorio');
+
+console.log(somatorioDivisiveis(10)); // Saída esperada: 23
+console.log(somatorioDivisiveis(11)); // Saída esperada: 33
---
+++ somatorio.js
@@ -0,0 +1,14 @@
+function somatorioDivisiveis(numero) {
+  let soma = 0;
+
+  for (let i = 3; i < numero; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      soma += i;
+    }
+  }
+
+  return soma;
+}
+
+module.exports = somatorioDivisiveis;
+
